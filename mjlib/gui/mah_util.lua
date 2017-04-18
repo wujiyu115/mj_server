9ebaa6c34f761734120a7c5c2c0b514a49afd60e
wujiyu <wujiyu115@gmail.com>
2017-04-18 16:54:12 +0800

    sync:bugfix

--- mjlib/gui/mah_util.lua
+++ mjlib/gui/mah_util.lua
@@ -22,51 +22,73 @@ function M.check_hu(t, laizi)
         count = count + v
     end
     if count%3 ~= 2 then
+        print(string.format("card count:%s", count))
         return false
     end
     return has_laizi and gui_hulib.get_hu_info(t, nil, laizi) or hulib.get_hu_info(t)
 end
 
 function M.check_ting(t, laizi)
+    local ting = {}
     local count = 0
-    local hand_cards_tmp = {}
+    local hand_tmp = {}
     for i,v in ipairs(t) do
         count = count + v
-        hand_cards_tmp[i] = v
+        hand_tmp[i] = v
     end
     if count%3 ~= 1 then
-        return false
+        return ting
     end
     for card = 1,34 do
-        hand_cards_tmp[card] = hand_cards_tmp[card] + 1
-        local can_hu = M.check_hu(hand_cards_tmp, laizi)
-        if can_hu then
-            print("听牌", card)
+        if laizi ~= card then
+            hand_tmp[card] = hand_tmp[card] + 1
+            local can_hu = M.check_hu(hand_tmp, laizi)
+            if can_hu then
+                utils.print_array(hand_tmp)
+                table.insert(ting, card)
+            end
+            hand_tmp[card] = hand_tmp[card] - 1
         end
-        hand_cards_tmp[card] = hand_cards_tmp[card] - 1
     end
+    return ting
 end
 
-local function test_one()
-    -- 6万6万6万4筒4筒4筒4条4条5条5条6条6条发发
-    local laizi = 34
+
+
+local function test_ting()
     -- local t = {
     --     0,0,0,   0,0,2,   0,0,0,
     --     0,0,0,   2,0,0,   0,0,0,
     --     0,0,0,   1,2,2,   0,0,0,
     --     0,0,0,0, 0,0,2}
-
     local t = {
         0,0,0,   0,0,0,   0,0,0,
         0,0,1,   1,2,1,   0,0,0,
         0,0,0,   2,2,2,   0,0,0,
-        0,0,0,0, 0,2,0}
-    -- print(M.check_hu(t, laizi))
-    print(M.check_ting(t, laizi))
+        0,0,0,0, 0,1,1}
+    local ting = M.check_ting(t, laizi)
+    utils.print(ting)
 
 end
 
-test_one()
+local laizi = 32
+local function test_hu()
+        -- local t = {
+    --     0,0,0,   0,0,2,   0,0,0,
+    --     0,0,0,   2,0,0,   0,0,0,
+    --     0,0,0,   1,2,2,   0,0,0,
+    --     0,0,0,0, 0,0,2}
+    local t = {
+            0,0,0, 0,0,0, 0,0,1,
+            1,0,1, 1,2,1, 0,0,0,
+            0,0,0, 2,2,2, 0,0,0,
+            0,0,0,0,1,0,0,}
+    print(M.check_hu(t, laizi))
+    -- print(gui_hulib.get_hu_info(t, nil, laizi))
+
+end
+
+test_hu()
 
 
 return M
